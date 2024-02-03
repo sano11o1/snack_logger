aef872c572c0c4757e802543f2d9390cad111151
Mashio Sano <52710925+sano11o1@users.noreply.github.com>
2024-02-04 08:32:06 +0900

    フォルダ階層を整えた

---
+++ logger/logger.go
@@ -0,0 +1,7 @@
+package logger
+
+import "fmt"
+
+func InitSnackLogger() {
+	fmt.Println("Snack logger started")
+}
---
+++ server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/sano11o1/snack_logger/logger"
+
+func main() {
+	logger.InitSnackLogger()
+}
